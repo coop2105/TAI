4dbb6744afe6f95454e39b9a06be3c19794b4814
HADUCTAI <pkxx2105@gmail.com>
2026-08-25 16:19:36 +0700

    sau

--- TAI/Program.cs
+++ TAI/Program.cs
@@ -1,9 +1,16 @@
-﻿internal class Program
+﻿internal class ex02
 {
-    private static void Main(string[] args)
+    static void ex01()
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("nanana");
-        Console.WriteLine("iku");
+        Console.WriteLine("nhap do c");
+        float ce = float.Parse(Console.ReadLine());
+        float ke = ce + 273;
+        float fa = ce * 18 / 10 + 32;
+        Console.WriteLine($"{ce} c = {ke} k = {fa} f"); 
     }
+    static void Main()
+    {
+        ex01();
+    }
+
 }
